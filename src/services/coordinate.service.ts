e3d15b97980327f886bf4647c63259d0f1051d4f
max <lettnermaximilian2008@gmail.com>
2026-03-19 10:10:32 +0100

    feat: started implementing the WebSocket

--- src/services/coordinate.service.ts
+++ src/services/coordinate.service.ts
@@ -5,19 +5,23 @@ export default interface coord {
   timestamp: string
 }
 
-// const socket = new WebSocket('ws://10.10.0.190:8000/api/stream/coordinates')
+let latestCoord : coord
 
-// socket.addEventListener('message', (event) => {
-//   console.log(event.data)
-// })
+const socket = new WebSocket('ws://10.10.0.190:8000/api/stream/coordinates')
+
+socket.addEventListener('message', (event) => {
+  latestCoord = event.data
+  //console.log(latestCoord)
+})
 
 export const getLatestPosition = (): coord => {
-  //not yet finished, currently just sample code
+  return latestCoord;
     
   return { x: 100, y: 100, z: Math.random() * 200, timestamp:'2026-02-20T17:56:56.939303+00:00'}
 }
 
 export const getScale = (): number => {
+  //fetch(10.10.0.190:8000/api/scale)
   return 200
   //TODO: implement scale
 }
